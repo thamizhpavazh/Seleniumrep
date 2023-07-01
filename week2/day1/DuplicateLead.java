769e396b25223634773404d5316714dbc0de30da
thamizhpavazh <44086030+thamizhpavazh@users.noreply.github.com>
2023-07-01 22:06:07 +0530

    week2.day1assignments

---
+++ week2/day1/DuplicateLead.java
@@ -0,0 +1,44 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DuplicateLead {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.partialLinkText("CRM")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("testleaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("pavazhthamizhanna");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("P");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("thamizh");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Training");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("Jul23Batch3");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("pavazh@gmail.com");
+		WebElement statePR=driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select sec= new Select(statePR);
+		sec.selectByVisibleText("New York");
+		driver.findElement(By.className("smallSubmit")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
+		Thread.sleep(10);
+		driver.findElement(By.linkText("Duplicate Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).clear();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("New testleaf");
+		driver.findElement(By.id("createLeadForm_firstName")).clear();
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("PTA");
+		driver.findElement(By.className("smallSubmit")).click();
+		String title2 = driver.getTitle();
+		System.out.println(title2);
+	}
+
+}
---
+++ week2/day1/EditLead.java
@@ -0,0 +1,42 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class EditLead {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.partialLinkText("CRM")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("testleaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("pavazhthamizhanna");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("P");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("thamizh");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Training");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("Jul23Batch3");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("pavazh@gmail.com");
+		WebElement statePR=driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select sec= new Select(statePR);
+		sec.selectByVisibleText("New York");
+		driver.findElement(By.className("smallSubmit")).click();
+		Thread.sleep(10);
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_description")).clear();
+		driver.findElement(By.id("updateLeadForm_importantNote")).sendKeys("Updated now");
+		driver.findElement(By.className("smallSubmit")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+	}
+
+}
